c9317b65136bcd7c579bffd291558c07521f8fd9
teafr <tookyoumy7@gmail.com>
2025-09-11 18:38:51 +0300

    initialized progect and created dbase functionality. Replaced getters, setters and constructors with lombok annotations

---
+++ practic_1/src/main/java/org/example/Cart.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Cart {
+    private List<Product> products;
+
+    public Cart() {
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Кошик містить:\n");
+        for (Product product : products) {
+            sb.append(product.toString()).append("\n");
+        }
+        sb.append("Загальна вартість: ").append(getTotalPrice());
+        return sb.toString();
+    }
+}
---
+++ practic_1/src/main/java/org/example/Main.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Category electronics = new Category(1, "Електроніка");
+        Category smartphones = new Category(2, "Смартфони");
+        Category accessories = new Category(3, "Аксесуари");
+
+        Product product1 = new Product(1, "Ноутбук", 19999.99, "Високопродуктивний ноутбук для роботи та ігор", electronics);
+        Product product2 = new Product(2, "Смартфон", 12999.50, "Смартфон з великим екраном…", smartphones);
+        Product product3 = new Product(3, "Навушники", 2499.00, "Бездротові навушники з шумозаглушенням", accessories);
+
+        Cart cart = new Cart();
+
+        while (true) {
+            System.out.println("\nВиберіть опцію:");
+            System.out.println("1 - Переглянути список товарів");
+            System.out.println("2 - Додати товар до кошика");
+            System.out.println("3 - Переглянути кошик");
+            System.out.println("4 - Зробити замовлення");
+            System.out.println("0 - Вийти");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println(product1);
+                    System.out.println(product2);
+                    System.out.println(product3);
+                    break;
+                case 2:
+                System.out.println("Введіть ID товару для додавання до кошика:");
+                int id = scanner.nextInt();
+
+                if (id == 1) cart.addProduct(product1);
+                else if (id == 2) cart.addProduct(product2);
+                else if (id == 3) cart.addProduct(product3);
+                else System.out.println("Товар з таким ID не знайдено");
+                break;
+                case 3:
+                    System.out.println(cart);
+                    break;
+                case 4:
+                    System.out.println("Замовлення оформлено!");
+                    cart = new Cart();
+                    break;
+                case 0:
+                    System.out.println("Дякуємо, що використовували наш магазин!");
+                    return;
+                default:
+                    System.out.println("Невідома опція. Спробуйте ще раз.");
+                    break;
+            }
+
+        }
+    }
+}
