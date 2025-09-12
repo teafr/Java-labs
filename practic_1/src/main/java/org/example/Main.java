a6e4b88c98fe107486ea739ae014c8c8a3c58c4c
teafr <tookyoumy7@gmail.com>
2025-09-12 13:46:21 +0300

    Made "find by name/category" functionality

--- practic_1/src/main/java/org/example/Main.java
+++ practic_1/src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,9 +13,10 @@ public class Main {
         Category smartphones = new Category(2, "Смартфони");
         Category accessories = new Category(3, "Аксесуари");
 
-        Product product1 = new Product(1, "Ноутбук", 19999.99, "Високопродуктивний ноутбук для роботи та ігор", electronics);
-        Product product2 = new Product(2, "Смартфон", 12999.50, "Смартфон з великим екраном…", smartphones);
-        Product product3 = new Product(3, "Навушники", 2499.00, "Бездротові навушники з шумозаглушенням", accessories);
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1, "Ноутбук", 19999.99, "Високопродуктивний ноутбук для роботи та ігор", electronics));
+        products.add(new Product(2, "Смартфон", 12999.50, "Смартфон з великим екраном…", smartphones));
+        products.add(new Product(3, "Навушники", 2499.00, "Бездротові навушники з шумозаглушенням", accessories));
 
         Cart cart = new Cart();
         List<Order> orders = new ArrayList<>();
@@ -27,23 +29,25 @@ public class Main {
             System.out.println("4 - Переглянути кошик");
             System.out.println("5 - Зробити замовлення");
             System.out.println("6 - Переглянути історію замовлень");
+            System.out.println("7 - Знайти товар");
             System.out.println("0 - Вийти");
 
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println(product1);
-                    System.out.println(product2);
-                    System.out.println(product3);
+                    for (Product product : products) System.out.println(product);
                     break;
                 case 2:
                     System.out.println("Введіть ID товару для додавання до кошика:");
                     int id = scanner.nextInt();
 
-                    if (id == 1) cart.addProduct(product1);
-                    else if (id == 2) cart.addProduct(product2);
-                    else if (id == 3) cart.addProduct(product3);
-                    else System.out.println("Товар з таким ID не знайдено");
+                    Product productToAdd = products.stream().findFirst().filter(product -> product.getId() == id).orElse(null);
+                    if (productToAdd == null) {
+                        System.out.println("Нема товару за цим ID");
+                        break;
+                    }
+
+                    cart.addProduct(productToAdd);
                     break;
                 case 3:
                     System.out.println("Який товар хочете видалити з кошика? Напишіть ID");
@@ -79,6 +83,33 @@ public class Main {
                     System.out.println("Історія замовлень:");
                     for (Order order : orders) {
                         System.out.println(order);
+                    }
+                    break;
+                case 7:
+                    System.out.println("Знайти за назвою (1) або категорією (2)? Щоб повернутись до меню, натисніть 0");
+                    int option = scanner.nextInt();
+
+                    switch (option) {
+                        case 1:
+                            System.out.println("Напишіть назіу товару:");
+                            String name = scanner.next();
+
+                            List<Product> foundProductsByName = products.stream().filter(product -> product.getName().equals(name)).toList();
+                            for (Product product : foundProductsByName) System.out.println(product);
+                            break;
+                        case 2:
+                            System.out.println("Напишіть категорію товару:");
+                            String category = scanner.next();
+
+                            List<Product> foundProductsByCategory = products.stream().filter(product -> product.getCategory().getName().equals(category)).toList();
+                            for (Product product : foundProductsByCategory) System.out.println(product);
+                            break;
+                        case 0:
+                            System.out.println("Повертаємось до основного меню");
+                            return;
+                        default:
+                            System.out.println("Невідома опція.");
+                            break;
                     }
                     break;
                 case 0:
